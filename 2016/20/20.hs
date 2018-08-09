6803d52d14276e69d6f22bdba68db7887ccb0fa8
Simen de Lange <simen.de.lange@gmail.com>
2018-08-09 15:58:34 +0200

    Made running time of HS solution 1/20

--- 2016/20/20.hs
+++ 2016/20/20.hs
@@ -2,21 +2,25 @@
 
 import Data.List (permutations, splitAt)
 
--- Splits a number string at a specified position. For example, `splitNumStr "1234567890" 3` returns (123,4567890)
-splitNumStr :: String -> Int -> (Int,Int)
-splitNumStr numStr n = (read a, read b)
-    where (a,b) = splitAt n numStr
+-- | Builds a number from a list of digits
+buildNum :: [Int] -> Int
+buildNum digits = foldl (\num d -> d + num*10) 0 digits
 
--- Splits a number string in all possible locations. "1234567890" -> [(1,234567890), (12,34567890), (123,4567890)...]
-allPossibleNumStrSplits :: String -> [(Int,Int)]
-allPossibleNumStrSplits numStr = map (splitNumStr numStr) [1..9]
+-- Splits a number at a specified position. For example, `splitNum 1234567890 3` returns (123,4567890)
+splitNum :: [Int] -> Int -> (Int,Int)
+splitNum digits n = (buildNum a, buildNum b)
+    where (a,b) = splitAt n digits
+
+-- Splits a number in all possible locations. 1234567890 -> [(1,234567890), (12,34567890), (123,4567890)...]
+allPossibleNumSplits :: [Int] -> [(Int,Int)]
+allPossibleNumSplits digits = map (splitNum digits) [1..9]
 
 -- All possible pair of numbers which can be created from the digits 0-9
 pairs :: [(Int,Int)]
 pairs = concat
-        . map allPossibleNumStrSplits
+        . map allPossibleNumSplits
         . permutations
-        $ ['0'..'9']
+        $ [0..9]
 
 -- List of all products of the pairs
 products :: [Int]
@@ -27,3 +31,4 @@ alvinsNumber :: Int
 alvinsNumber = maximum products
 
 main = putStrLn $ show alvinsNumber
+
